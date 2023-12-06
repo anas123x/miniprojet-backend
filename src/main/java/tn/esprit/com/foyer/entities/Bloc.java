60e4d18f61993a4b990c17609b1796da1a206ec4
anas123x <anas.dai@esprit.tn>
2023-12-06 23:00:20 +0100

    Revert "Merge branch 'feriel2' into origin"
    This reverts commit 07cb264c96dd4bbf3a0d7c1a973b37763e700aa0, reversing
    changes made to 763640314cf1680528a1a65804f002617dcd3e48.

---
+++ src/main/java/tn/esprit/com/foyer/entities/Bloc.java
@@ -0,0 +1,34 @@
+package tn.esprit.com.foyer.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "Bloc")
+public class Bloc implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idFoyer")
+    private Long idFoyer; // Clé primaire
+    private String nomBloc;
+    private Long capaciteBloc;
+    @ManyToOne
+    Foyer foyer;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="bloc", fetch = FetchType.EAGER)
+
+    private Set<Chambre> chambres;
+
+
+}
